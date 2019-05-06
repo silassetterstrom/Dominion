28e7242b5406fe08d9694ef6e475744203757212
silas-setterstrom <43864054+silas-setterstrom@users.noreply.github.com>
2019-05-05 20:56:34 -0500

    allows for card replacement

--- src/Randomize.java
+++ src/Randomize.java
@@ -36,6 +36,134 @@ public class Randomize {
 		return randomized;
 	}
 	
+	public static ArrayList<Card> replaceCard(ArrayList<Expansion> kingdom, ArrayList<Card> cardSet, int index) {
+		Randomize randObject = new Randomize(kingdom);
+		ArrayList<Card> possibleCards = randObject.getAllCards();
+		
+		int acceptFlag = 0;
+		while(acceptFlag == 0) {
+			int rand = (int)Math.floor(Math.random() * (possibleCards.size()));
+			Card card = possibleCards.get(rand);
+			int match = 0;
+			for(int i = 0; i < cardSet.size(); i++) {
+				if(card.getName().equals(cardSet.get(i).getName())) {
+					match++;
+				}
+			}
+			if(match == 0) {
+				cardSet.remove(index);
+				cardSet.add(card);
+				acceptFlag++;
+			}
+		}
+		
+		return cardSet;
+	}
+	
+	public static ArrayList<Card> sortCards(ArrayList<Card> cardSet) {
+		ArrayList<Card> alphabetized = new ArrayList<Card>();
+		
+		while(!cardSet.isEmpty()) {
+			Card first = cardSet.get(0);
+			int removeIndex = 0;
+			for(int i = 1; i < cardSet.size(); i++) {
+				if(cardSet.get(i).getName().compareTo(first.getName()) < 0) {
+					removeIndex = i;
+					first = cardSet.get(i);
+				}
+			}
+			alphabetized.add(first);
+			cardSet.remove(removeIndex);
+		}
+		
+		ArrayList<Card> sortedProperly = new ArrayList<Card>();
+		
+		while(!alphabetized.isEmpty()) {
+			Card first = alphabetized.get(0);
+			int removeIndex = 0;
+			for(int i = 1; i < alphabetized.size(); i++) {
+				if(alphabetized.get(i).getCost() < first.getCost()) {
+					first = alphabetized.get(i);
+					removeIndex = i;
+				}
+			}
+			sortedProperly.add(first);
+			alphabetized.remove(removeIndex);
+		}
+		
+		return sortedProperly;
+	}
+	
+	public static void userLoop(ArrayList<Expansion> kingdom) {
+		System.out.println("Welcome to the Dominion Randomizer. Here are the loaded expansions:");
+		for(int i = 0; i < kingdom.size(); i++) {
+			System.out.println((i+1) + ") " + kingdom.get(i).getName());
+		}
+		System.out.println("Are there any you'd like to exclude? Enter the number" + 
+				" of the expansion to remove it or 0 to continue.");
+		
+		int exitFlag = 1;
+		Scanner scan = new Scanner(System.in);
+		
+		while(exitFlag == 1) {
+			try {
+				int remove = scan.nextInt();
+				while(remove != 0) {
+					String removed = kingdom.get(remove -1).getName();
+					kingdom.remove(remove - 1);
+					System.out.println("Removed expansion " + removed);
+					System.out.println("Would you like to remove another? If so, enter its new number."
+							+ " If not, enter 0.");
+					for(int i = 0; i < kingdom.size(); i++) {
+						System.out.println((i+1) + ") " + kingdom.get(i).getName());
+					}
+					remove = scan.nextInt();
+				}
+
+			System.out.print("Randomizing with expansions: ");
+			for(int i = 0; i < kingdom.size(); i++) {
+				System.out.print(kingdom.get(i).getName() + ". ");
+			} 
+		
+			System.out.println("\n");
+			System.out.println("Here is your randomized set: ");
+			Randomize gen = new Randomize(kingdom);
+			ArrayList<Card> randomizedCards = gen.randGen();
+			randomizedCards = sortCards(randomizedCards);
+			for(int i = 0; i < randomizedCards.size(); i++) {
+				System.out.print((i+1) + ") ");
+				randomizedCards.get(i).print();
+			}
+			
+			System.out.println("Would you like to replace a card? Enter the number of the card if "
+					+ "yes, otherwise enter 0.");
+			int cardNumber = scan.nextInt();
+			while(cardNumber != 0) {
+				randomizedCards = replaceCard(kingdom, randomizedCards, cardNumber-1);
+				System.out.println("Card replaced. Here is your new set: ");
+				randomizedCards = sortCards(randomizedCards);
+				for(int i = 0; i < randomizedCards.size(); i++) {
+					System.out.print((i+1) + ") ");
+					randomizedCards.get(i).print();
+				}
+				System.out.println("Would you like to replace another card? Enter the number of "
+						+ "the card if yes, otherwise enter 0.");
+				cardNumber = scan.nextInt();
+			}
+			
+			System.out.println("Would you like to randomize again? Enter '1' to randomize again"
+					+ " or '0' to exit.");
+			exitFlag = scan.nextInt();
+		} catch (Exception e) {
+			System.out.println("Error: Incorrect input.");
+			scan.close();
+			return;
+			}
+		}
+		scan.close();
+		return;
+	}
+	
 	public static void main(String[] args) {
 		ArrayList<Expansion> kingdom = new ArrayList<Expansion>();
 		Expansion testExp1 = new Expansion();
@@ -54,47 +182,9 @@ public class Randomize {
 		testExp3.setName("Dark Ages");
 		kingdom.add(testExp1);
 		kingdom.add(testExp2);
-		
 		kingdom.add(testExp3);
-		System.out.println("Welcome to the Dominion Randomizer. Here are the loaded expansions:");
-		for(int i = 0; i < kingdom.size(); i++) {
-			System.out.println((i+1) + ") " + kingdom.get(i).getName());
-		}
-		System.out.println("Are there any you'd like to exclude? Enter the number" + 
-				" of the expansion to remove it or 0 to continue.");
 		
-		Scanner scan = new Scanner(System.in);
-		try {
-			int remove = scan.nextInt();
-			while(remove != 0) {
-				String removed = kingdom.get(remove -1).getName();
-				kingdom.remove(remove - 1);
-				System.out.println("Removed expansion " + removed);
-				System.out.println("Would you like to remove another? If so, enter its new number."
-						+ " If not, enter 0.");
-				for(int i = 0; i < kingdom.size(); i++) {
-					System.out.println((i+1) + ") " + kingdom.get(i).getName());
-				}
-				remove = scan.nextInt();
-			}
-		} catch (Exception e) {
-			System.out.println("Error: Please enter the number of one of the expansions,"
-					+ " or 0.");
-		}
-
-		System.out.print("Randomizing with expansions: ");
-		for(int i = 0; i < kingdom.size(); i++) {
-			System.out.print(kingdom.get(i).getName() + ". ");
-		} 
-		
-		System.out.println("\n");
-		System.out.println("Here is your randomized set: ");
-		Randomize gen = new Randomize(kingdom);
-		ArrayList<Card> testCards = gen.randGen();
-		for(int i = 0; i < testCards.size(); i++) {
-			testCards.get(i).print();
-		}
-
-		scan.close();
+		userLoop(kingdom);
+		System.out.println("Thank you for using the Dominion Randomizer! The program will now exit.");
 	}
 }
